a45894cf70d81e317bf0081d4aadca0f63b4615c
KevinG64-cloud <atonementandreconciliation@gmail.com>
2026-04-25 21:16:43 -0500

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -4,9 +4,9 @@ import { fileURLToPath } from "node:url";
 const projectRoot = fileURLToPath(new URL(".", import.meta.url));
 
 const nextConfig: NextConfig = {
-  //turbopack: {
-    //root: projectRoot,
-  //},
+  turbopack: {
+    root: projectRoot,
+  },
 };
 
 export default nextConfig;
